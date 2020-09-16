127f7262dd66a76ee0cf9e8fe95dc8f1fce77da9
Will Bollock <wbollock@gmail.com>
2020-09-16 11:24:08 -0400

    web kind of works

--- sleeper.php
+++ sleeper.php
@@ -1,19 +1,29 @@
 <html>
 <body>
 
+<?php
+$username = "";
+if(isset($_POST['submit'])){ //check if form was submitted
+    $username = $_POST['name'];
+    $command = 'python3 sleeperPy.py '.$username;
+    #$script = escapeshellcmd('python3 sleeperPy.py ').$username;
+    #$output = shell_exec($command);
+    #readfile("tiers.txt");
+    exec($command);
+    header( "Location: tiers.txt" );
+    #echo file_get_contents("tiers.txt");
+}
+?>
 
-<form action="#" method="post">
+
+
+<form action="" method="post">
 Sleeper Username: <input type="text" name="name"><br>
 <input type="submit" name="submit" value="submit">
 </form>
 
-<?php
-$username = $_POST['name'];
-$script = escapeshellcmd('python3 sleeper.Py ').$username;
-$output = shell_exec($command);
-echo $output;
 
-?>
+
 
 </body>
 </html>
